c9bc01281288cde86a0ac54bebe8445e1c0e7b04
Jeremy Swinnen <hey@jeremys.be>
2023-12-16 22:14:29 +0100

    Adding parallax debug logic

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 //import Header from "./Header";
 import Footer from "./Footer";
+import ParallaxDebug from "./ParallaxDebug";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -9,6 +10,7 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <main className="mx-auto w-4/5">
+      <ParallaxDebug />
       {/* <Header /> */}
       {/* Page wrapper */}
       <section className="grid grid-cols-8">{children}</section>
--- src/components/ParallaxDebug.tsx
+++ src/components/ParallaxDebug.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 
-interface ParallaxDebugProps {
-  // Define your props here
-  // Example: title: string;
-}
+const ParallaxDebug = () => {
+  const children = Array.from({ length: 10 }, (_, index) => (
+    <div key={index} className="relative col-span-1 flex items-end justify-end">
+      {index !== 9 && ( // Conditional rendering: Exclude span for the 10th element
+        <span className="absolute -bottom-3 right-2 z-20 rounded-full bg-black/80 p-2 py-0.5 text-xs tracking-wider text-white backdrop-blur-lg font-medium">
+          {`${(index + 1) * 10}%`}
+        </span>
+      )}
+    </div>
+  ));
 
-const ParallaxDebug: React.FC<ParallaxDebugProps> = (
-  {
-    /* destructure your props here */
-  },
-) => {
-  return <div>{/* Your component content goes here */}</div>;
+  return (
+    <div className="fixed left-0 top-0 z-50 grid h-full w-full grid-cols-1 divide-y divide-black/20 bg-transparent">
+      {children}
+    </div>
+  );
 };
 
 export default ParallaxDebug;

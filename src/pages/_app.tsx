b2ecf112abcd73db7598468f4a9ad77219cb5f1c
Jeremy Swinnen <hey@jeremys.be>
2023-12-26 14:42:38 +0100

    Adding Lenis smooth scroll globally (disabled for now)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import { useEffect } from "react";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Lenis from "@studio-freight/lenis";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      lerp: 0.15,
+    });
+
+    const raf = (time: number) => {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <>
       <Layout>

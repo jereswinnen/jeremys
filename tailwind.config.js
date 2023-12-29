cf46349121c0bfc55305f887aea79f169da78ef7
Jeremy Swinnen <hey@jeremys.be>
2023-12-29 13:55:31 +0100

    Adding 'Display' font-face to Tailwind config and fixing font-stacks

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,9 @@ module.exports = {
       'normal': '380ms',
     },
     fontFamily: {
-      sans: ["Instrument Sans", "Georgia", "sans-serif"],
-      serif: ["Instrument Serif", "Arial", "Georgia", "serif"],
+      sans: ["Instrument Sans", "-apple-system", "BlinkMacSystemFont", "avenir next", "avenir", "segoe ui", "helvetica neue", "helvetica", "Cantarell", "Ubuntu", "roboto", "noto", "arial", "sans-serif"],
+      serif: ["Instrument Serif", "Iowan Old Style", "Apple Garamond", "Baskerville", "Times New Roman", "Droid Serif", "Times", "Source Serif Pro", "serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"],
+      display: ["Humane", "-apple-system", "BlinkMacSystemFont", "avenir next", "avenir", "segoe ui", "helvetica neue", "helvetica", "Cantarell", "Ubuntu", "roboto", "noto", "arial", "sans-serif"],
     },
     fontSize: {
       '2xs': '0.625rem', //10px

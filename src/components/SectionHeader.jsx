4736f42f4d1de7bb81bf5f1b24faf0017a82ffbe
Jeremy Swinnen <hey@jeremys.be>
2024-12-31 15:45:30 +0100

    Tweaking header component styling

--- src/components/SectionHeader.jsx
+++ src/components/SectionHeader.jsx
@@ -3,10 +3,10 @@ import { IconContext } from "phosphor-react";
 
 export default function SectionHeader({ icon: Icon, text }) {
   return (
-    <div className="col-span-full pt-2 border-t-2 border-bg-black/10 font-medium flex items-center gap-2">
+    <div className="col-span-full pt-2 border-t-2 border-bg-black/10 font-medium flex items-center gap-1.5">
       {Icon && (
         <IconContext.Provider value={{ size: 20, weight: "duotone" }}>
-          <Icon />
+          <Icon className="opacity-50" />
         </IconContext.Provider>
       )}
       <p>{text}</p>

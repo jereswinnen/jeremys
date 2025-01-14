7813b0354f7138b4fcdc43d0cb85e70c54504988
Jeremy Swinnen <hey@jeremys.be>
2025-01-15 00:24:27 +0100

    Adding project name in scroller and adjusting appearance

--- src/components/WorkDisplay.tsx
+++ src/components/WorkDisplay.tsx
@@ -42,11 +42,11 @@ export default function WorkDisplay({ work, className }: WorkDisplayProps) {
   );
 
   return (
-    <div className={`bg-emerald-10 ${className || ""}`}>
+    <div className={`${className || ""}`}>
       <div
         ref={ref}
         {...events}
-        className="flex gap-4 no-scrollbar overflow-x-scroll cursor-grab active:cursor-grabbing"
+        className="flex gap-2 py-4 no-scrollbar overflow-x-scroll cursor-grab active:cursor-grabbing"
       >
         {allImages.map((image, index) => {
           const basePath = getProjectImagePath(
@@ -54,16 +54,21 @@ export default function WorkDisplay({ work, className }: WorkDisplayProps) {
             image.src
           ).replace(/\.[^/.]+$/, "");
           return (
-            <picture key={index} className="flex-none">
-              <source srcSet={`${basePath}.avif`} type="image/avif" />
-              <source srcSet={`${basePath}.webp`} type="image/webp" />
-              <img
-                src={`${basePath}.png`}
-                alt={image.caption || `Image from ${image.projectName}`}
-                loading="lazy"
-                className="pointer-events-none max-h-[60vh] object-contain select-none"
-              />
-            </picture>
+            <div className="group flex-none">
+              <span className="block transition-all duration-500 ease-in-circ opacity-0 translate-y-2 blur-sm group-hover:opacity-100 group-hover:-translate-y-2 group-hover:blur-none">
+                {image.projectName}
+              </span>
+              <picture key={index} className="flex-non">
+                <source srcSet={`${basePath}.avif`} type="image/avif" />
+                <source srcSet={`${basePath}.webp`} type="image/webp" />
+                <img
+                  src={`${basePath}.png`}
+                  alt={image.caption || `Image from ${image.projectName}`}
+                  loading="lazy"
+                  className="pointer-events-none max-h-[60vh] object-contain select-none bg-zinc-100 dark:bg-zinc-900 p-6"
+                />
+              </picture>
+            </div>
           );
         })}
       </div>

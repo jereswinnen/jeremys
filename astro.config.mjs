66f759e0772921003cb481180bc92c8e096c84d2
Jeremy Swinnen <hey@jeremys.be>
2025-01-29 23:31:07 +0100

    Adding Swup for smooth page transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import swup from "@swup/astro";
 
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), swup()],
   vite: {
     ssr: {
-      noExternal: ['gsap'],
+      noExternal: ["gsap"],
     },
   },
 });
